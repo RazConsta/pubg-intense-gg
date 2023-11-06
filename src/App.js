595d96a94cacf98dd627d61d3f33f927d0a3f2e8
Raz Consta <66283215+RazConsta@users.noreply.github.com>
2023-11-05 23:40:42 -0800

    added 404.html and changed baseroute

--- src/App.js
+++ src/App.js
@@ -14,32 +14,32 @@ import ErangelSeverny from './ErangelSeverny';
 
 function App() {
   return (
-    <Router>
+    <Router basename='/pubg-intense-gg'>
       <div className="App">
         <nav className="navbar">
           <img src={[pubgLogo]} alt="PUBG Logo" className="logo" />
           <img src={[ibrLogo]} alt="IBR Logo" className="logo" />
           <h1 className="title">Survival Guide</h1>
           <ul className="nav-links">
-            <li className="nav-item"><a href="/basics">Basics</a></li>
-            <li className="nav-item"><a href="/erangel">Erangel</a></li>
-            <li className="nav-item"><a href="/miramar">Miramar</a></li>
-            <li className="nav-item"><a href="/sanhok">Sanhok</a></li>
-            <li className="nav-item"><a href="/vikendi">Vikendi</a></li>
-            <li className="nav-item"><a href="/taego">Taego</a></li>
-            <li className="nav-item"><a href="/deston">Deston</a></li>
+            <li className="nav-item"><a href="/pubg-intense-gg/basics">Basics</a></li>
+            <li className="nav-item"><a href="/pubg-intense-gg/erangel">Erangel</a></li>
+            <li className="nav-item"><a href="/pubg-intense-gg/miramar">Miramar</a></li>
+            <li className="nav-item"><a href="/pubg-intense-gg/sanhok">Sanhok</a></li>
+            <li className="nav-item"><a href="/pubg-intense-gg/vikendi">Vikendi</a></li>
+            <li className="nav-item"><a href="/pubg-intense-gg/taego">Taego</a></li>
+            <li className="nav-item"><a href="/pubg-intense-gg/deston">Deston</a></li>
           </ul>
         </nav>
         <MainPageContent/>
         <Routes>
-          <Route path="/basics" element={<MainPageContent />} />
-          <Route path="/erangel" element={<Erangel />} />
-          <Route path="/erangel/severny" element={<ErangelSeverny />} />
-          <Route path="/miramar" element={<Miramar />} />
-          <Route path="/sanhok" element={<Sanhok />} />
-          <Route path="/vikendi" element={<Vikendi />} />
-          <Route path="/deston" element={<Deston />} />
-          <Route path="/taego" element={<Taego />} />
+          <Route path="/pubg-intense-gg/basics" element={<MainPageContent />} />
+          <Route path="/pubg-intense-gg/erangel" element={<Erangel />} />
+          <Route path="/pubg-intense-gg/erangel/severny" element={<ErangelSeverny />} />
+          <Route path="/pubg-intense-gg/miramar" element={<Miramar />} />
+          <Route path="/pubg-intense-gg/sanhok" element={<Sanhok />} />
+          <Route path="/pubg-intense-gg/vikendi" element={<Vikendi />} />
+          <Route path="/pubg-intense-gg/deston" element={<Deston />} />
+          <Route path="/pubg-intense-gg/taego" element={<Taego />} />
         </Routes>
       </div>
     </Router>
