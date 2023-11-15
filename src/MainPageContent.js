2e74f610dc65d5dab8901390d5b0fb4dda406160
Raz Consta <66283215+RazConsta@users.noreply.github.com>
2023-11-14 22:21:17 -0800

    added table count

--- src/MainPageContent.js
+++ src/MainPageContent.js
@@ -6,6 +6,67 @@ import MapsSection from "./MapsSection";
 import "./MainPageContent.css";
 
 function MainPageContent() {
+  const mapData = [
+    {
+      map: "Deston",
+      locations: [
+        { name: "Barclift", yellowBoxes: 5, blueBoxes: 0 },
+        { name: "Holston Meadows", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Hydroelectric Dam", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Lodge (+1 outside)", yellowBoxes: '✔️ 6', blueBoxes: 1 },
+        { name: "Swamp", yellowBoxes: 3, blueBoxes: 0 },
+      ],
+    },
+    {
+      map: "Erangel",
+      locations: [
+        { name: "Farm", yellowBoxes: 5, blueBoxes: 0 },
+        { name: "Georgopol", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Hospital", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Mansion", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Severny", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+      ],
+    },
+    {
+      map: "Miramar",
+      locations: [
+        { name: "El Azahar", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Los Leones", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Monte Nuevo", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Resort", yellowBoxes: 2, blueBoxes: 0 },
+        { name: "Water Treatment", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+      ],
+    },
+    {
+      map: "Sanhok",
+      locations: [
+        { name: "Bootcamp", yellowBoxes: '✔️ 4', blueBoxes: 0 },
+        { name: "Paradise Resort", yellowBoxes: '✔️ 4', blueBoxes: 0 },
+        { name: "Ruins", yellowBoxes: '✔️ 4', blueBoxes: 0 },
+      ],
+    },
+    {
+      map: "Taego",
+      locations: [
+        { name: "Airport", yellowBoxes: 5, blueBoxes: 0 },
+        { name: "Fishing Camp", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Go Dok", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Song Am", yellowBoxes: 5, blueBoxes: 0 },
+        { name: "Wol Song", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+      ],
+    },
+    {
+      map: "Vikendi",
+      locations: [
+        { name: "Coal Mine", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Lumber Yard", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Observatory", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Villa", yellowBoxes: '✔️ 6', blueBoxes: 0 },
+        { name: "Winery", yellowBoxes: 5, blueBoxes: 0 },
+      ],
+    },
+  ];
+
   return (
     <div>
       <div>
@@ -28,55 +89,30 @@ function MainPageContent() {
               <li>Phase 4 - + 1m</li>
               <li>Phase 5 - </li>
             </ul>
-            <h3>These are the number of yellow boxes I was able to map for each map (145 total):</h3>
-            <ul>
-              <li>Deston</li>
-              <ul>
-                <li>Barclift - 5</li>
-                <li>✔️ Holston Meadows - 6</li>
-                <li>✔️ Hydroelectric Dam - 6</li>
-                <li>✔️ Lodge - 6 (+1 outside)</li>
-                <li>Swamp - 3</li>
-              </ul>
-              <li>Erangel</li>
-              <ul>
-                <li> Farm - 5</li>
-                <li>✔️ Georgopol - 6</li>
-                <li>✔️ Hospital - 6</li>
-                <li>✔️ Mansion - 6</li>
-                <li>✔️ Severny - 6</li>
-              </ul>
-              <li>Miramar</li>
-              <ul>
-                <li>✔️ El Azahar - 6</li>
-                <li>✔️ Los Leones - 6</li>
-                <li>✔️ Monte Nuevo - 6</li>
-                <li>Resort - 2</li>
-                <li>✔️ Water Treatment - 6</li>
-              </ul>
-              <li>Sanhok</li>
-              <ul>
-                <li>✔️ Bootcamp - 4</li>
-                <li>✔️ Paradise Resort - 4</li>
-                <li>✔️ Ruins - 4</li>
-              </ul>
-              <li>Taego</li>
-              <ul>
-                <li>Airport - 5</li>
-                <li>✔️ Fishing Camp - 6</li>
-                <li>✔️ Go Dok - 6</li>
-                <li>Song Am - 5</li>
-                <li>✔️ Wol Song - 6</li>
-              </ul>
-              <li>Vikendi</li>
-              <ul>
-                <li>✔️ Coal Mine - 6</li>
-                <li>✔️ Lumber Yard - 6</li>
-                <li>✔️ Observatory - 6</li>
-                <li>✔️ Villa - 6</li>
-                <li>Winery - 5</li>
-              </ul>
-            </ul>
+            <h3>These are the number of yellow and blue boxes I was able to map so far:</h3>
+            {mapData.map((map, index) => (
+          <div key={index}>
+            <h4>{map.map}</h4>
+            <table>
+              <thead>
+                <tr>
+                  <th>Location</th>
+                  <th>Yellow Boxes</th>
+                  <th>Blue Boxes</th>
+                </tr>
+              </thead>
+              <tbody>
+                {map.locations.map((location, index) => (
+                  <tr key={index}>
+                    <td>{location.name}</td>
+                    <td>{location.yellowBoxes}</td>
+                    <td>{location.blueBoxes}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ))}
           </div>
           <div className="boxes">
             <div className="box">
